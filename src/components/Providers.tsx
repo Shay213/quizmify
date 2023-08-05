f7ca0225c286d4751ce2c01d4edaf8819ecbf315
shay213 <dawidpawelec@o2.pl>
2023-08-05 15:50:55 +0200

    Add theme toggle

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import React from "react";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-type Props = {
-  children: React.ReactNode;
-};
-
-const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <SessionProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        {...props}
+      >
+        {children}
+      </NextThemesProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
