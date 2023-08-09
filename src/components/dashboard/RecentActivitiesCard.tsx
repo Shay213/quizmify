494e2645c3a0a4ab2c6b6647d5c103643724dcf1
shay213 <dawidpawelec@o2.pl>
2023-08-09 17:46:54 +0200

    Add history component to dashboard page

--- src/components/dashboard/RecentActivitiesCard.tsx
+++ src/components/dashboard/RecentActivitiesCard.tsx
@@ -6,18 +6,34 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import QuizHistory from "../QuizHistory";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/db";
 
 type Props = {};
 
-const RecentActivitiesCard = (props: Props) => {
+const RecentActivitiesCard = async (props: Props) => {
+  const session = await getAuthSession();
+
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const gamesCount = await prisma.game.count({
+    where: { userId: session.user.id },
+  });
+
   return (
     <Card className="col-span-4 lg:col-span-3">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">Recent Activities</CardTitle>
-        <CardDescription>You have played a total of 7 games.</CardDescription>
+        <CardDescription>
+          You have played a total of {gamesCount} games.
+        </CardDescription>
       </CardHeader>
       <CardContent className="max-h-[580px] overflow-auto">
-        Histories
+        <QuizHistory limit={10} userId={session.user.id} />
       </CardContent>
     </Card>
   );
